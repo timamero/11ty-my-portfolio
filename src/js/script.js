ac4642ef511367cd24d0a4e66fc7a54f99dd45cd
Fatima <fcamero12@gmail.com>
2021-06-19 16:47:50 -0700

    Fix nav link container transition and add About link to main navbar

--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,20 @@ const burger = document.querySelector('.hamburger');
 const navLinks = document.getElementById('nav-links-container');
 const navLinksList = document.querySelectorAll('.nav__link')
 const contactNavLink = document.getElementById('contact-nav-link')
+const aboutNavLink = document.getElementById('about-nav-link')
 
 burger.addEventListener('click', function () {
   burger.classList.toggle('is-active');
   navLinks.classList.toggle('show-nav-links');
 });
 
-// Need to hide nav when Contact link is selected while in the home page
+// Need to hide nav when Contact and About link is selected while in the home page
 contactNavLink.addEventListener('click', function () {
+  burger.classList.toggle('is-active');
+  navLinks.classList.toggle('show-nav-links');
+});
+
+aboutNavLink.addEventListener('click', function () {
   burger.classList.toggle('is-active');
   navLinks.classList.toggle('show-nav-links');
 });
