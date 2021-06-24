c16ba53dd2199ae64eec4b7b563c4f48c1fb69c7
Fatima <fcamero12@gmail.com>
2021-06-24 13:54:18 -0700

    Revise script to fix Uncaught TypeError on event listener

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@ const navLinks = document.getElementById('nav-links-container');
 const navLinksList = document.querySelectorAll('.nav__link')
 const contactNavLink = document.getElementById('contact-nav-link')
 const aboutNavLink = document.getElementById('about-nav-link')
+const contactForm = document.getElementById('contact-form')
 
 
 burger.addEventListener('click', function () {
@@ -11,15 +12,19 @@ burger.addEventListener('click', function () {
 });
 
 // Need to hide nav when Contact and About link is selected while in the home page
-contactNavLink.addEventListener('click', function () {
+if (contactNavLink) {
+  contactNavLink.addEventListener('click', function () {
   burger.classList.toggle('is-active');
   navLinks.classList.toggle('show-nav-links');
 });
+}
 
-aboutNavLink.addEventListener('click', function () {
+if (aboutNavLink) {
+  aboutNavLink.addEventListener('click', function () {
   burger.classList.toggle('is-active');
   navLinks.classList.toggle('show-nav-links');
 });
+}
 
 
 // Need to clear form after it is submitted and user returns to home 
@@ -27,8 +32,10 @@ const nameField = document.getElementById('name');
 const emailField = document.getElementById('email');
 const messageField = document.getElementById('message');
 
-window.addEventListener('load', (event) => {
+if (contactForm) {
+  window.addEventListener('load', (event) => {
   nameField.value = ''
   emailField.value = ''
   messageField.value = ''
 });
+}
